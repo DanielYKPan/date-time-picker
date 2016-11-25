a4a7d4dd2eda13b20b3a709752ef6d8c57da79f6
Yaokun Pan <myron.yk.pan@gmail.com>
2016-11-25 13:44:47 +1100

    Created date-picker.component time-picker.component modal.component moment.pipe

---
+++ index.ts
@@ -0,0 +1,5 @@
+/**
+ * index
+ */
+
+export * from './src/date-time-picker/date-time-picker.module';
---
+++ src/date-time-picker/index.ts
@@ -0,0 +1,9 @@
+/**
+ * index
+ */
+
+export * from './picker-modal/modal.component';
+export * from './pipes/moment.pipe';
+export * from './date-picker/date-picker.component';
+export * from './time-picker/time-picker.component';
+export * from './date-time-picker.module';
