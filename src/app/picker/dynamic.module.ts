0fa75ed374d51eac2c18a1ca59492255200090fb
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-20 10:40:31 +1100

    Move moment.pipe modal.component to picker folder

--- src/app/picker/dynamic.module.ts
+++ src/app/picker/dynamic.module.ts
@@ -5,8 +5,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DialogComponent } from './dialog.component';
-import { ModalComponent } from '../picker-modal/modal.component';
-import { MomentPipe } from "../pipes/moment.pipe";
+import { ModalComponent } from './modal.component';
+import { MomentPipe } from "./moment.pipe";
 import { DatePanelComponent } from './date-panel.component';
 import { TimePanelComponent } from './time-panel.component';
 import { SlideControlComponent } from './slider.component';
