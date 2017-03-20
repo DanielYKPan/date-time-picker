84289103ab77abcd465481aa611f80d9f834621f
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-20 17:30:34 +1100

    Added web-animation-js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,6 +4,10 @@
 
 import 'core-js/es6';
 import 'core-js/es7/reflect';
+
+// Web animations for Safari and Firefox #10420
+import 'web-animations-js';
+
 require('zone.js/dist/zone');
 if (process.env.ENV === 'production') {
     // Production
