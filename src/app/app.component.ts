a1dfaa46f54b70100baa22faa0e5d8349b651cf8
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-20 19:51:20 +1100

    Added demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,14 @@ export class AppComponent implements OnInit {
 
     private moment: Date;
 
+    private input1Moment: any;
+    private input2Moment: any;
+    private input3Moment: any;
+    private input4Moment: any;
+    private input5Moment: any;
+    private input6Moment: any;
+    private input7Moment: any;
+
     constructor() {
     }
 
