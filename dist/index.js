7f53ce75698af879791aa46b1ea696662bb50b32
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-26 16:03:41 +1100

    Change base-font-size from rem to px

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var picker_module_1 = require("./lib/picker.module");
-exports.DateTimePickerModule = picker_module_1.DateTimePickerModule;
-var picker_directive_1 = require("./lib/picker.directive");
-exports.DateTimePickerDirective = picker_directive_1.DateTimePickerDirective;
-//# sourceMappingURL=index.js.map
