a01d417ac4f9c256ae222aa113222d16cab8bbb8
Yaokun Pan <myron.yk.pan@gmail.com>
2017-04-21 12:19:47 +1000

    Added ro ru locale

---
+++ src/app/locale/ru.ts
@@ -0,0 +1,14 @@
+/**
+ * ru - Russian
+ */
+
+export const LANG_RU_TRANS = {
+    'time picker': 'Выбор Времени',
+    'today': 'Сегодня',
+    'set time': 'Установить Время',
+    'close': 'Закрыть',
+    'confirm': 'Подтвердить',
+    'hour': 'Час',
+    'minute': 'Минута',
+    'second': 'Секунда',
+};
