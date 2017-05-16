9758cab4b11eaf81786fc29ad8dd186d687fade4
nrbw <nicolas.remy@banquewormser.com>
2017-05-16 14:42:41 +0200

    Update French translation

--- src/locale/fr.ts
+++ src/locale/fr.ts
@@ -3,9 +3,9 @@
  */
 
 export const LANG_FR_TRANS = {
-    'time picker': 'Choix du temps',
+    'time picker': 'Choix de l\'heure',
     'today': 'Aujourd\'hui',
-    'set time': 'Modifier l\'heure',
+    'set time': 'Confirmer l\'heure',
     'close': 'Fermer',
     'confirm': 'Confirmer',
     'hour': 'Heure',
