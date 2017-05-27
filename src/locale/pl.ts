3b2a68f256d0486983a6bdec1898dc3c5cb0d06d
Yaokun Pan <myron.yk.pan@gmail.com>
2017-05-27 18:49:13 +1000

    Added pl locale(polish)

---
+++ src/locale/pl.ts
@@ -0,0 +1,16 @@
+/**
+  pl - Polish
+ */
+
+export const LANG_PL_TRANS = {
+  'time picker': 'Wybór czasu',
+  'today': 'Dziś',
+  'set time': 'Ustaw czas',
+  'close': 'Zamknij',
+  'confirm': 'Potwierdź',
+  'hour': 'Godzina',
+  'minute': 'Minuta',
+  'second': 'Sekunda',
+};
+
+
