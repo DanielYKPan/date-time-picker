2fc80abb18077bc9aecb330cd8c8757def3677cf
Joao Paulo <joao.paulo@viasoft.com.br>
2017-05-29 15:43:06 -0300

    Add Portuguese translations

--- src/translations.ts
+++ src/translations.ts
@@ -12,6 +12,7 @@ import { LANG_LT_TRANS } from './locale/lt';
 import { LANG_PL_TRANS } from './locale/pl';
 import { LANG_RO_TRANS } from './locale/ro';
 import { LANG_RU_TRANS } from './locale/ru';
+import { LANG_PT_TRANS } from './locale/pt';
 import { LANG_ZHCN_TRANS } from './locale/zh-cn';
 import { LANG_ZHHK_TRANS } from './locale/zh-hk';
 import { LANG_ZHTW_TRANS } from './locale/zh-tw';
@@ -28,6 +29,7 @@ export const dictionary = {
     'pl': LANG_PL_TRANS,
     'ro': LANG_RO_TRANS,
     'ru': LANG_RU_TRANS,
+    'pt': LANG_PT_TRANS,
     'zh_CN': LANG_ZHCN_TRANS,
     'zh_HK': LANG_ZHHK_TRANS,
     'zh_TW': LANG_ZHTW_TRANS,
