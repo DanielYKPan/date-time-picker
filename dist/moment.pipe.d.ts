b9c96440f09f1b0f32fa9457f634659a60c4860e
Yaokun Pan <myron.yk.pan@gmail.com>
2017-07-17 17:07:24 +1000

    Update package to version 4.2.3

--- dist/moment.pipe.d.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import { PickerService } from './picker.service';
-import { Moment } from 'moment/moment';
-export declare class MomentPipe implements PipeTransform {
-    private service;
-    constructor(service: PickerService);
-    transform(moment: Moment, format?: string): string;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
-import { ColorPickerModule } from 'angular2-color-picker';
-import { DateTimePickerModule } from '../../npmdist/picker.module';
+import { DateTimePickerModule } from 'ng-pick-datetime';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         DateTimePickerModule,
-        ColorPickerModule,
     ],
     declarations: [
         AppComponent
