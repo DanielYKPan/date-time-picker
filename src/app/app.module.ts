76e13629ddc64fb6ddf8046d146e916400bf7aaf
Yaokun Pan <myron.yk.pan@gmail.com>
2017-09-01 16:31:34 +1000

    Used new picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
-import { DateTimePickerModule } from '../../npmdist/picker.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateTimePickerModule } from 'ng-pick-datetime';
 
 @NgModule({
     imports: [
