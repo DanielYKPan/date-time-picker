996a069c799848009c370f03f34a0d9e7c3a9a29
Jim Cullison <jim.cullison@gmail.com>
2017-09-13 10:01:43 -0600

    Allow configuring the increment for minutes to be other than 1, default to 1.

--- src/picker.component.ts
+++ src/picker.component.ts
@@ -281,6 +281,13 @@ export class DateTimePickerComponent implements OnInit, AfterViewInit, OnDestroy
     @Input() showOtherMonths: boolean = true;
 
     /**
+     * Minute increment
+     * @default 1
+     * @type {number}
+     */
+    @Input() minuteIncrement = 1;
+
+    /**
      * Callback to invoke when dropdown gets focus.
      * */
     @Output() onFocus = new EventEmitter<any>();
@@ -707,9 +714,9 @@ export class DateTimePickerComponent implements OnInit, AfterViewInit, OnDestroy
 
         let minutes = getMinutes(value);
         if (val === 'increase') {
-            minutes += 1;
+            minutes += this.minuteIncrement;
         } else if (val === 'decrease') {
-            minutes -= 1;
+            minutes -= this.minuteIncrement;
         } else {
             minutes = val;
         }
