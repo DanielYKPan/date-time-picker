71360b4b7d9bc18b17e3e6defb55af501f7db020
Yaokun Pan <myron.yk.pan@gmail.com>
2018-01-15 11:43:24 +1100

    Updated date-tim index

--- src/date-time/index.ts
+++ src/date-time/index.ts
@@ -5,5 +5,9 @@
 export { OwlDateTimeModule } from './date-time.module';
 export { OwlDateTimeIntl } from './date-time-picker-intl.service';
 export { OwlNativeDateTimeModule } from './adapter/native-date-time.module';
-export { OWL_DATE_TIME_LOCALE, DateTimeAdapter } from './adapter/date-time-adapter.class';
-export { OWL_DATE_TIME_FORMATS } from './adapter/date-time-format.class';
+export {
+    OWL_DATE_TIME_LOCALE_PROVIDER,
+    OWL_DATE_TIME_LOCALE,
+    DateTimeAdapter
+} from './adapter/date-time-adapter.class';
+export { OWL_DATE_TIME_FORMATS, OwlDateTimeFormats } from './adapter/date-time-format.class';
