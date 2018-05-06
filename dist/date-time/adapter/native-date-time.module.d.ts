2dea67293cac939818b85cd18622748650b8345f
Yaokun Pan <myron.yk.pan@gmail.com>
2018-05-06 23:56:02 +1000

    used picker version 6.0.0

--- dist/date-time/adapter/native-date-time.module.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class NativeDateTimeModule {
-}
-export declare class OwlNativeDateTimeModule {
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from '../../npmdist';
-// import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+// import { OwlDateTimeModule, OwlNativeDateTimeModule } from '../../npmdist';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 // import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
 import { MatTabsModule } from '@angular/material';
 import { CodeHighlightDirective } from './code-highlight.directive';
