2c6db8ee09589e6743cf122c9af1f3102e671955
Frédéric JAMMES <fjammes@sqli.com>
2020-03-13 19:09:49 +0100

    Update to Angular 9

--- projects/picker/src/public_api.ts
+++ projects/picker/src/public_api.ts
@@ -1,7 +1,6 @@
 /**
  * picker
  */
-
 export { OwlDateTimeModule } from './lib/date-time/date-time.module';
 export { OwlDateTimeIntl } from './lib/date-time/date-time-picker-intl.service';
 export { OwlNativeDateTimeModule } from './lib/date-time/adapter/native-date-time.module';
@@ -13,3 +12,10 @@ export {
 export { OWL_DATE_TIME_FORMATS, OwlDateTimeFormats } from './lib/date-time/adapter/date-time-format.class';
 export { OwlDateTimeInlineComponent } from './lib/date-time/date-time-inline.component';
 export { OwlDateTimeComponent } from './lib/date-time/date-time-picker.component';
+export { OwlCalendarComponent } from './lib/date-time/calendar.component';
+export { OwlTimerComponent } from './lib/date-time/timer.component';
+export { OwlDateTimeTriggerDirective } from './lib/date-time/date-time-picker-trigger.directive';
+export { OwlDateTimeInputDirective } from './lib/date-time/date-time-picker-input.directive';
+export { OwlMultiYearViewComponent } from './lib/date-time/calendar-multi-year-view.component';
+export { OwlYearViewComponent } from './lib/date-time/calendar-year-view.component';
+export { OwlMonthViewComponent } from './lib/date-time/calendar-month-view.component';
