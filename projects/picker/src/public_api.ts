392eba9f23570302fc8ef78b11112b73347a067d
Pawel Paulinski <pawel.paulinski.bc@gmail.com>
2022-01-12 21:51:20 +0100

    feat: public API update

--- projects/picker/src/public_api.ts
+++ projects/picker/src/public_api.ts
@@ -2,14 +2,20 @@
  * picker
  */
 
-export { OwlDateTimeModule } from './lib/date-time/date-time.module';
-export { OwlDateTimeIntl } from './lib/date-time/date-time-picker-intl.service';
-export { OwlNativeDateTimeModule } from './lib/date-time/adapter/native-date-time.module';
 export {
-  OWL_DATE_TIME_LOCALE_PROVIDER,
-  OWL_DATE_TIME_LOCALE,
-  DateTimeAdapter
+  DateTimeAdapter, OWL_DATE_TIME_LOCALE, OWL_DATE_TIME_LOCALE_PROVIDER
 } from './lib/date-time/adapter/date-time-adapter.class';
-export { OWL_DATE_TIME_FORMATS, OwlDateTimeFormats } from './lib/date-time/adapter/date-time-format.class';
+export { OwlDateTimeFormats, OWL_DATE_TIME_FORMATS } from './lib/date-time/adapter/date-time-format.class';
+export { OwlNativeDateTimeModule } from './lib/date-time/adapter/native-date-time.module';
+export { OwlMonthViewComponent } from './lib/date-time/calendar-month-view.component';
+export { OwlMultiYearViewComponent } from './lib/date-time/calendar-multi-year-view.component';
+export { OwlYearViewComponent } from './lib/date-time/calendar-year-view.component';
+export { OwlCalendarComponent } from './lib/date-time/calendar.component';
 export { OwlDateTimeInlineComponent } from './lib/date-time/date-time-inline.component';
+export { OwlDateTimeInputDirective } from './lib/date-time/date-time-picker-input.directive';
+export { OwlDateTimeIntl } from './lib/date-time/date-time-picker-intl.service';
+export { OwlDateTimeTriggerDirective } from './lib/date-time/date-time-picker-trigger.directive';
 export { OwlDateTimeComponent } from './lib/date-time/date-time-picker.component';
+export { OwlDateTimeModule } from './lib/date-time/date-time.module';
+export { OwlTimerComponent } from './lib/date-time/timer.component';
+
